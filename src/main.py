da8ed876040b7017370f73d4837ac59b7dcd815a
rdouglascanada <rdouglascanada@gmail.com>
2023-05-25 19:16:23 -0400

    0.4.0 Add boundaries, backgrounds, and score text to play state

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from rich2d.game import Game, GameConfig
-from rich2d.models import Model
 from rich2d.models.state import State, StateModel
 from title_model import SnakeTitleModel
+from play_model import SnakePlayModel
 
 window_width = 600
 window_height = 700
@@ -10,8 +10,9 @@ game_config = GameConfig(window_width=window_width, window_height=window_height,
 
 game_state = State(value="title")
 title_model = SnakeTitleModel(game_state, window_width, window_height)
+play_model = SnakePlayModel(game_state, window_width, window_height)
 
-game_state_map = {'title': title_model, 'play': Model()}
+game_state_map = {'title': title_model, 'play': play_model}
 game_model = StateModel(state=game_state, state_map=game_state_map)
 
 snake_game = Game(model=game_model, config=game_config)
