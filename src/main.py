169ecdf3dae6087a09c8e2aba73e2ac9f64ec19b
rdouglascanada <rdouglascanada@gmail.com>
2023-05-25 18:08:05 -0400

    0.3.0 Ability to switch to Play state when spacebar is pressed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 from rich2d.game import Game, GameConfig
+from rich2d.models import Model
+from rich2d.models.state import State, StateModel
 from title_model import SnakeTitleModel
 
 window_width = 600
 window_height = 700
-snake_config = GameConfig(window_width=window_width, window_height=window_height,
-                          window_title="Snake", background_colour="white")
+game_config = GameConfig(window_width=window_width, window_height=window_height,
+                         window_title="Snake", background_colour="white")
 
-title_model = SnakeTitleModel(window_width, window_height)
-snake_game = Game(model=title_model, config=snake_config)
+game_state = State(value="title")
+title_model = SnakeTitleModel(game_state, window_width, window_height)
+
+game_state_map = {'title': title_model, 'play': Model()}
+game_model = StateModel(state=game_state, state_map=game_state_map)
+
+snake_game = Game(model=game_model, config=game_config)
 snake_game.run()
--- src/title_model.py
+++ src/title_model.py
@@ -1,11 +1,13 @@
+import pygame
 from rich2d.models import Model
 from rich2d.elements.grid import Grid, GridElement
 from rich2d.elements.animated_text import FlashingText
 from rich2d.sprites import Text
+from rich2d.handlers import KeyboardHandler
 
 
 class SnakeTitleModel(Model):
-    def __init__(self, window_width, window_height):
+    def __init__(self, game_state, window_width, window_height):
         title_grid = Grid(rect=(0, 0, window_width, window_height), rows=10, columns=10)
         title_text = Text(rect=(0, 0, 0, 0), text="Snake", colour="green",
                           font_size=160, font_bold=True, font_name="courier",
@@ -18,7 +20,13 @@ class SnakeTitleModel(Model):
         spacebar_text_grid_element = GridElement(sprite=spacebar_text, grid=title_grid, grid_tiles=(1, 5, 8, 1))
         flashing_text_element = FlashingText(text_sprite=spacebar_text, flash_interval=2, flashes_per_interval=1)
 
+        def handle_spacebar_press():
+            game_state.set_value("play")
+            return
+        keyboard_handler = KeyboardHandler(key_pressed_map={pygame.K_SPACE: handle_spacebar_press})
+
         sprites = [title_text, spacebar_text]
         elements = [title_text_grid_element, spacebar_text_grid_element, flashing_text_element]
-        super().__init__(sprites=sprites, elements=elements)
+        handlers = [keyboard_handler]
+        super().__init__(sprites=sprites, elements=elements, handlers=handlers)
         return
