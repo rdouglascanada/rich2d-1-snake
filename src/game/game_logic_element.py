04fc7efd9559b02e3a587d5951bbdbe656013fff
rdouglascanada <rdouglascanada@gmail.com>
2023-05-26 20:42:34 -0400

    0.9.0 Add Game Over substate, sprites, elements, and handler

--- src/game/game_logic_element.py
+++ src/game/game_logic_element.py
@@ -7,8 +7,10 @@ from rich2d.elements.grid import GridTiles
 class GameLogicElement(Element):
     UPDATE_INTERVAL = 0.25
 
-    def __init__(self, play_grid=None, snake_model=None,
+    def __init__(self, play_state=None, play_grid=None, snake_model=None,
                  boundary_tiles_list=None, food_tile=None, score_element=None):
+        if play_state is None:
+            raise RuntimeError("GameLogicElement play_state cannot be None")
         if play_grid is None:
             raise RuntimeError("GameLogicElement play_grid cannot be None")
         if snake_model is None:
@@ -20,17 +22,25 @@ class GameLogicElement(Element):
         if score_element is None:
             raise RuntimeError("GameLogicElement score_element cannot be None")
 
+        self._play_state = play_state
         self._play_grid = play_grid
         self._snake_model = snake_model
         self._boundary_tiles_list = boundary_tiles_list
         self._food_tile = food_tile
         self._score_element = score_element
         self._update_timestamp = time.time()
-        self._game_over = False
+        return
+
+    def new_game(self):
+        self._play_state.set_value("play")
+        self._snake_model.start_game()
+        self._place_food_tile()
+        self._score_element.set_score(0)
+        self._update_timestamp = time.time()
         return
 
     def update(self):
-        if self._game_over:
+        if self._play_state.get_value() == "game_over":
             return
 
         current_time = time.time()
@@ -38,19 +48,21 @@ class GameLogicElement(Element):
             self._update_timestamp = current_time
             tile_in_front_of_snake = GridTiles(values=self._snake_model.get_tile_in_front())
             for boundary_tiles in self._boundary_tiles_list:
-                if boundary_tiles.collides_with(tile_in_front_of_snake) \
-                        or self._snake_model.collides_with(tile_in_front_of_snake):
-                    self._game_over = True
+                if boundary_tiles.collides_with(tile_in_front_of_snake):
+                    self._play_state.set_value("game_over")
                     return
+            if self._snake_model.collides_with(tile_in_front_of_snake):
+                self._play_state.set_value("game_over")
+                return
             if self._food_tile.collides_with(tile_in_front_of_snake):
                 self._snake_model.grow()
                 self._score_element.set_score(self._score_element.get_score() + 1)
-                self.place_food_tile()
+                self._place_food_tile()
             else:
                 self._snake_model.move()
         return
 
-    def place_food_tile(self):
+    def _place_food_tile(self):
         self._food_tile.set_x_index(random.randint(1, self._play_grid.get_columns() - 2))
         self._food_tile.set_y_index(random.randint(1, self._play_grid.get_columns() - 2))
         while self._snake_model.collides_with(self._food_tile):
--- src/play_screen.py
+++ src/play_screen.py
@@ -1,7 +1,10 @@
+import pygame
 from rich2d.models import Model, ModelGroup
+from rich2d.models.state import State, StateModel
 from rich2d.sprites.shapes import Rectangle, Circle
 from rich2d.elements.grid import Grid, GridElement
 from rich2d.sprites import Text
+from rich2d.handlers import KeyboardHandler
 from game import ScoreElement, SnakeModel, GameLogicElement
 
 
@@ -9,6 +12,7 @@ def snake_play_screen(window_width, window_height):
     play_rows = 20
     play_columns = 20
     play_grid = Grid(rect=(0, 0, window_width, window_height - 100), rows=play_rows, columns=play_columns)
+    play_state = State(value="play")
 
     play_background = Rectangle(rect=(0, 0, 0, 0), colour="yellow")
     play_background_element = GridElement(sprite=play_background, grid=play_grid,
@@ -42,21 +46,61 @@ def snake_play_screen(window_width, window_height):
     score_text_grid_element = GridElement(sprite=score_text, grid=status_grid,
                                           grid_tiles=(6, 0, 1, 2))
     score_sync_element = ScoreElement(score_text_sprite=score_text, score=0)
+    top_instructions_text = Text(text="Use the arrow keys", rect=(0, 0, 0, 0), colour="black",
+                                 font_name="courier", font_size=28,
+                                 horizontal_alignment=Text.HorizontalAlignment.LEFT)
+    top_instructions_text_grid_element = GridElement(sprite=top_instructions_text, grid=status_grid,
+                                                     grid_tiles=(9, 0, 10, 1))
+    bottom_instructions_text = Text(text="to move the snake", rect=(0, 0, 0, 0), colour="black",
+                                    font_name="courier", font_size=28,
+                                    horizontal_alignment=Text.HorizontalAlignment.LEFT)
+    bottom_instructions_text_grid_element = GridElement(sprite=bottom_instructions_text, grid=status_grid,
+                                                        grid_tiles=(9, 1, 10, 1))
+
+    top_game_over_text = Text(text="Game Over", rect=(0, 0, 0, 0), colour="black",
+                              font_name="courier", font_size=28, font_bold=True,
+                              horizontal_alignment=Text.HorizontalAlignment.LEFT)
+    top_game_over_text_grid_element = GridElement(sprite=top_game_over_text, grid=status_grid,
+                                                  grid_tiles=(9, 0, 10, 1))
+    bottom_game_over_text = Text(text="Press spacebar to play again", rect=(0, 0, 0, 0), colour="black",
+                                 font_name="courier", font_size=18,
+                                 horizontal_alignment=Text.HorizontalAlignment.LEFT)
+    bottom_game_over_text_grid_element = GridElement(sprite=bottom_game_over_text, grid=status_grid,
+                                                     grid_tiles=(9, 1, 10, 1))
 
     snake_model = SnakeModel(play_grid=play_grid)
     snake_model.start_game()
     boundary_tiles_list = tuple([top_boundary_element.get_grid_tiles(), left_boundary_element.get_grid_tiles(),
                                  right_boundary_element.get_grid_tiles(), bottom_boundary_element.get_grid_tiles()])
-    game_logic_element = GameLogicElement(play_grid=play_grid, snake_model=snake_model,
+    game_logic_element = GameLogicElement(play_grid=play_grid, play_state=play_state,
+                                          snake_model=snake_model,
                                           boundary_tiles_list=boundary_tiles_list,
                                           food_tile=food_element.get_grid_tiles(),
                                           score_element=score_sync_element)
 
-    sprites = [play_background, top_boundary, left_boundary, right_boundary, bottom_boundary,
-               score_label_text, score_text, food_sprite]
-    elements = [play_background_element, top_boundary_element, left_boundary_element,
-                right_boundary_element, bottom_boundary_element, food_element,
-                score_label_grid_element, score_text_grid_element, game_logic_element, score_sync_element]
-    handlers = []
-    play_model = ModelGroup(models=[Model(sprites=sprites, elements=elements, handlers=handlers), snake_model])
-    return play_model
+    play_sprites = [play_background, top_boundary, left_boundary, right_boundary, bottom_boundary,
+                    score_label_text, score_text, top_instructions_text, bottom_instructions_text, food_sprite]
+    play_elements = [play_background_element, top_boundary_element, left_boundary_element,
+                     right_boundary_element, bottom_boundary_element, food_element,
+                     score_label_grid_element, score_text_grid_element, game_logic_element, score_sync_element,
+                     top_instructions_text_grid_element, bottom_instructions_text_grid_element]
+    play_handlers = []
+    play_model = ModelGroup(models=[Model(sprites=play_sprites, elements=play_elements, handlers=play_handlers),
+                                    snake_model])
+    game_over_sprites = [play_background, top_boundary, left_boundary, right_boundary, bottom_boundary,
+                         score_label_text, score_text, top_game_over_text, bottom_game_over_text, food_sprite]
+    game_over_elements = [play_background_element, top_boundary_element, left_boundary_element,
+                          right_boundary_element, bottom_boundary_element, food_element,
+                          score_label_grid_element, score_text_grid_element, score_sync_element,
+                          top_game_over_text_grid_element, bottom_game_over_text_grid_element]
+
+    def handle_spacebar():
+        game_logic_element.new_game()
+        return
+
+    key_pressed_map = {pygame.K_SPACE: handle_spacebar}
+    game_over_handlers = [KeyboardHandler(key_pressed_map=key_pressed_map)]
+    game_over_model = ModelGroup(models=[Model(sprites=game_over_sprites, elements=game_over_elements,
+                                               handlers=game_over_handlers), snake_model])
+    state_map = {'play': play_model, 'game_over': game_over_model}
+    return StateModel(state=play_state, state_map=state_map)
